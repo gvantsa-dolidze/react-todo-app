c1d3071ebba97535c8a7e668707a667f3242a120
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-07-18 00:04:45 +0400

    Create Button component with onClick handler for state update

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,22 @@
-import './Home.css'
+import { useState } from "react";
+import "./Home.css";
 const Home = () => {
+  const [number, setNumber] = useState(0);
+  // const decrement = () => {
+  //   setNumber(number => number -  1)
+  // }
+
+  // const increment = () => {
+  //   setNumber(number => number + 1)
+  // }
+
   return (
-    <div className='home'>Home</div>
-  )
-}
+    <div className="home">
+      <button onClick={() => setNumber((number) => number - 1)}>-</button>
+      <span>{number}</span>
+      <button onClick={() => setNumber((number) => number + 1)}>+</button>
+    </div>
+  );
+};
 
 export default Home;
