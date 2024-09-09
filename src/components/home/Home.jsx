173905a09a26faa46ee3e567d728fcb1c459be68
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-09-09 21:52:01 +0400

    create counter

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,20 +1,26 @@
 import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import "./Home.css";
+import { decrement, increment } from "../../store/counterSlice";
 const Home = () => {
   const [number, setNumber] = useState(0);
-  // const decrement = () => {
-  //   setNumber(number => number -  1)
-  // }
 
-  // const increment = () => {
-  //   setNumber(number => number + 1)
-  // }
+  const counter = useSelector(state => state.counterPage.counter);
+  const dispatch = useDispatch();
+
 
   return (
     <div className="home">
-      <button onClick={() => setNumber((number) => number - 1)}>-</button>
-      <span>{number}</span>
-      <button onClick={() => setNumber((number) => number + 1)}>+</button>
+      <div>
+        <button onClick={() => setNumber((number) => number - 1)}>-</button>
+        <span>{number}</span>
+        <button onClick={() => setNumber((number) => number + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={() => dispatch(decrement())}>-</button>
+        <span>{counter}</span>
+        <button onClick={() => dispatch(increment())}>+</button>
+      </div>
     </div>
   );
 };
---
+++ src/store/counterSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: {
+        counter: 0
+    },
+    reducers: {
+        increment: (state, action) => {
+            state.counter += 1;
+        },
+        decrement: (state, action) => {
+            state.counter -= 1;
+        }
+    }
+})
+
+export const { increment, decrement } = counterSlice.actions;
+export default counterSlice.reducer;
